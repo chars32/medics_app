61997f15f36f09925fb6f621c871aa3064158349
chars32 <chars24@gmail.com>
2023-10-17 23:50:48 -0600

    Push sin state

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:medics_app/api/firebase_api.dart';
 import 'package:medics_app/config/router/app_router.dart';
 import 'package:medics_app/config/theme/app_theme.dart';
+import 'package:medics_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
   runApp(const MainApp());
 }
 
